5c3c99494ed2b59b846632c6d2e5f0e3d275c9b6
lucas solari <solarilucas92@gmail.com>
2024-07-16 16:35:15 +0200

    m

--- src/store/AppContext.jsx
+++ src/store/AppContext.jsx
@@ -4,9 +4,18 @@ import { useContext, createContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+
+  const [condition, setCondition] = useState(true)
+  const [information, setInformation] = useState({})
+  const [loggedIn, setLoggedIn] = useState(false)
+
   
-    const store = {}
-  const actions = {};
+  const store = {condition, information}
+  const actions = {setCondition, getContacts};
+
+  useEffect(() => {
+    getFetchProfileInfo()
+  }, [loggedIn])
 
   return (
     <AppContext.Provider value={{ store, actions }}>
@@ -18,3 +27,4 @@ export const AppProvider = ({ children }) => {
 const useAppContext = () => useContext(AppContext);
 
 export default useAppContext;
+
